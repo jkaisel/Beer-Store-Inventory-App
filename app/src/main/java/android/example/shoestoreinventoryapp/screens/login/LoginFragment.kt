f90c967203d6704325a64b93261a6619692017c8
Joanna Lesiak <jo.kaisel@gmail.com>
2020-11-16 16:17:18 +0100

    Created register logic

--- app/src/main/java/android/example/shoestoreinventoryapp/screens/login/LoginFragment.kt
+++ app/src/main/java/android/example/shoestoreinventoryapp/screens/login/LoginFragment.kt
@@ -9,6 +9,9 @@ import android.example.shoestoreinventoryapp.R
 import android.example.shoestoreinventoryapp.databinding.FragmentLoginBinding
 import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import timber.log.Timber
 
@@ -17,14 +20,47 @@ class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
     private lateinit var viewModel: LoginViewModel
 
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
-
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+        viewModel.eventEmptyFiled.observe(viewLifecycleOwner, Observer { isEmpty ->
+            if(isEmpty){
+                binding.tvLoginInfo.text = "Fields email and password cannot be empty"
+                binding.tvLoginInfo.visibility = View.VISIBLE
+            }
+        })
+
+        viewModel.eventNotAnEmail.observe(viewLifecycleOwner, Observer {notEmail ->
+            if(notEmail){
+                binding.tvLoginInfo.text = "Please enter a valid email"
+                binding.tvLoginInfo.visibility = View.VISIBLE
+            }
+        })
+
+        viewModel.eventUserAlreadyExists.observe(viewLifecycleOwner, Observer { userExists ->
+            if(userExists){
+                binding.tvLoginInfo.text = "Email already registered"
+                binding.tvLoginInfo.visibility = View.VISIBLE
+            }
+        })
+
+        binding.btnRegister.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+            binding.tvLoginInfo.visibility = View.INVISIBLE
+            viewModel.resetEvents()
+
+            if(viewModel.checkFields(email, password) && viewModel.checkIfUserDoesntExist(email)){
+                viewModel.addNewUser(email, password)
+            }
+        }
 
 
         return binding.root
--- app/src/main/java/android/example/shoestoreinventoryapp/screens/login/LoginViewModel.kt
+++ app/src/main/java/android/example/shoestoreinventoryapp/screens/login/LoginViewModel.kt
@@ -1,8 +1,70 @@
 package android.example.shoestoreinventoryapp.screens.login
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
+    private val userList = mutableMapOf<String, String>()
 
+    private val _email = MutableLiveData<String>()
+    val email: LiveData<String>
+        get() = _email
+
+    private val _password = MutableLiveData<String>()
+    val password: LiveData<String>
+        get() = _password
+
+    private val _eventEmptyField = MutableLiveData<Boolean>()
+    val eventEmptyFiled: LiveData<Boolean>
+        get() = _eventEmptyField
+
+    private val _eventNotAnEmail = MutableLiveData<Boolean>()
+    val eventNotAnEmail: LiveData<Boolean>
+        get() = _eventNotAnEmail
+
+    private val _eventUserAlreadyExists = MutableLiveData<Boolean>()
+    val eventUserAlreadyExists : LiveData<Boolean>
+        get() = _eventUserAlreadyExists
+
+
+
+    init {
+        _eventEmptyField.value = false
+        _eventNotAnEmail.value = false
+        _eventUserAlreadyExists.value = false
+    }
+
+
+    fun checkFields(email: String, password: String): Boolean{
+        if(email.isEmpty() || password.isEmpty()) {
+            _eventEmptyField.value = true
+            return false
+        }
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            _eventNotAnEmail.value = true
+            return false
+        }
+
+        return true
+    }
+
+    fun checkIfUserDoesntExist(email: String): Boolean{
+        if(userList.containsKey(email)){
+            _eventUserAlreadyExists.value = true
+            return false
+        }
+        return true
+    }
+
+    fun addNewUser(email: String, password: String){
+        userList.put(email, password)
+    }
+
+    fun resetEvents(){
+        _eventEmptyField.value = false
+        _eventNotAnEmail.value = false
+        _eventUserAlreadyExists.value = false
+    }
 }
