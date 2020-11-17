a0b34f23d5df378625ce58a7b2febc46715c9db7
Joanna Lesiak <jo.kaisel@gmail.com>
2020-11-17 12:57:31 +0100

    Created basic fragment and layout

---
+++ app/src/main/java/android/example/shoestoreinventoryapp/screens/login/welcome/WelcomeFragment.kt
@@ -0,0 +1,25 @@
+package android.example.shoestoreinventoryapp.screens.login.welcome
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.example.shoestoreinventoryapp.R
+import android.example.shoestoreinventoryapp.databinding.FragmentWelcomeBinding
+import androidx.databinding.DataBindingUtil
+
+class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_welcome, container, false)
+
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+}
