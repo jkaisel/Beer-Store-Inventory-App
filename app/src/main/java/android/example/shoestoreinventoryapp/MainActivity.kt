866653cf2aa780487781de3abb826b37157735b5
Joanna Lesiak <jo.kaisel@gmail.com>
2020-11-19 20:30:10 +0100

    Added toolbar

--- app/src/main/java/android/example/shoestoreinventoryapp/MainActivity.kt
+++ app/src/main/java/android/example/shoestoreinventoryapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package android.example.shoestoreinventoryapp
 
+import android.example.shoestoreinventoryapp.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
