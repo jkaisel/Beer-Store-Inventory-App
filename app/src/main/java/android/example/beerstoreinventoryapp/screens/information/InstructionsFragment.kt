72f74df43fba3eb4aa759721583be296223d0f5a
Joanna Lesiak <jo.kaisel@gmail.com>
2020-11-26 18:43:57 +0100

    Login UI done

--- app/src/main/java/android/example/beerstoreinventoryapp/screens/information/InstructionsFragment.kt
+++ app/src/main/java/android/example/beerstoreinventoryapp/screens/information/InstructionsFragment.kt
@@ -3,21 +3,21 @@ package android.example.beerstoreinventoryapp.screens.information
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.example.beerstoreinventoryapp.R
-import android.example.beerstoreinventoryapp.databinding.FragmentInformationBinding
+import android.example.beerstoreinventoryapp.databinding.FragmentInstructionsBinding
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 
-class InformationFragment : Fragment() {
+class InstructionsFragment : Fragment() {
 
-    private lateinit var binding: FragmentInformationBinding
+    private lateinit var binding: FragmentInstructionsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_information, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_instructions, container, false)
 
         binding.btnGotIt.setOnClickListener {
             requireView().findNavController().navigate(R.id.action_informationFragment_to_beerListFragment )
